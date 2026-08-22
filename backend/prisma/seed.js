30debce29257e30afbe05e3a0859bd55577f01cf
Heri Irawan <heriira129@gmail.com>
2026-08-22 13:19:00 +0700

    feat: update url homepage

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -4,10 +4,10 @@ const prisma = new PrismaClient();
 async function main() {
   const providers = [
     { source: 'antam', display_name: 'Antam', type: 'fisik', logo: '/logos/antam.png', url_homepage: 'https://www.logammulia.com' },
-    { source: 'ubs', display_name: 'UBS', type: 'fisik', logo: '/logos/ubs.png', url_homepage: 'https://www.ubs-gold.com' },
+    { source: 'ubs', display_name: 'UBS', type: 'fisik', logo: '/logos/ubs.png', url_homepage: 'https://ubslifestyle.com/fine-gold/logam-mulia-ubs' },
     { source: 'galeri24', display_name: 'Galeri24', type: 'fisik', logo: '/logos/galery24.png', url_homepage: 'https://galeri24.co.id' },
-    { source: 'lotus-archi', display_name: 'Lotus Archi', type: 'fisik', logo: '/logos/lotus-archi.png', url_homepage: 'https://www.lotusarchi.com' },
-    { source: 'emasku', display_name: 'Emasku', type: 'fisik', logo: '/logos/emasku.png', url_homepage: 'https://www.emasku.co.id' },
+    { source: 'lotus-archi', display_name: 'Lotus Archi', type: 'fisik', logo: '/logos/lotus-archi.png', url_homepage: 'https://lotusarchi.com' },
+    { source: 'emasku', display_name: 'Emasku', type: 'fisik', logo: '/logos/emasku.png', url_homepage: 'https://www.emasku.co.id/id' },
     { source: 'treasury', display_name: 'Treasury', type: 'digital', logo: '/logos/treasury.png', url_homepage: 'https://treasury.id' },
     { source: 'pegadaian', display_name: 'Pegadaian Digital', type: 'digital', logo: '/logos/pegadaian.png', url_homepage: 'https://sahabat.pegadaian.co.id' },
     { source: 'cermati', display_name: 'Cermati', type: 'digital', logo: '/logos/cermati.png', url_homepage: 'https://www.cermati.com' },
